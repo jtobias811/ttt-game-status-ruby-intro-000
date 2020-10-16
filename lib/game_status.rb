dc9185da70c0f35c81cbe42232a26badfbccf2e6
Jacob Tobias <jtobias811@gmail.com>
2020-10-16 16:31:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,8 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    }
+    end
+  }
       return false
     end
   end
